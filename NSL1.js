d0080ba2c15c3e070a45c4cc8a3aec29d1b6cf64
july41776 <53682759+july41776@users.noreply.github.com>
2019-08-04 03:19:21 -0400

    Update NSL1.js

--- NSL1.js
+++ NSL1.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 /***menu league title and additional links********************************************************************************************/
-$( "a.navbar-brand" ).text( "National Simball League" );
+$( "a.navbar-brand" ).text( "National Simball Lg" );
 $("div#dlmaddenmenu ul.navbar-nav ").append('<a href="http://www.daddyleagues.com/NSL/admin/sync_week" class="btn btn-info ajax" role="button" data-method="post" data-disabled="true" data-loader=".ajax-loader-roster" data-html=".rosterresults">  <img class="ajax-loader-roster" style="display:none" alt="Sending ..." src="/img/ajax-loader.gif"> <em class="icon-wrench"></em></a>');
 
 /***scoreboard**********************/
